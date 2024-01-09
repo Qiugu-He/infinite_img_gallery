e83236361e28fadf07d167283580c825cf1345fe
Qiugu He <kaochi@Qius-MacBook-Pro.local>
2024-01-08 21:50:51 -0600

    upload from local repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import './App.css';
 
-function App() {
+const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
+
+export default function App() {
+  //state variable
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
+
+  useEffect(()=>{
+    getPhotos();
+  }, []);  // as soon as pages, this effect getPhotos run again 
+
+  function getPhotos() {
+    //normal api
+    let apiUrl = `https://api.unsplash.com/photos?`;
+
+    //seach api
+    if(query){
+      apiUrl = `https://api.unsplash.com/search/photos?query=${query}`;
+    }
+
+    apiUrl += `&page=${page}`;
+    apiUrl += `&client_id=${accessKey}`;
+
+    //fetch photos
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => {
+          const imagesFromApi = data.results ?? data;
+          // if page is 1, then we need a whole new array of images
+          if (page === 1) {
+            setImages(imagesFromApi);
+            return;
+          }
+
+          // if page > 1, then we are adding for our infinite scroll
+          setImages((images) => [...images, ...imagesFromApi]);
+      });
+  }
+
+  function searchPhotos(e) {
+    e.preventDefault(); // as html form automatically refresh browser, when submit, so need to prevent event
+    setPage(1);
+    getPhotos();
+  }
+
+  //return an erro if no access key for the images
+  if (!accessKey) {
+    return (
+      <a href="https://unsplash.com/developers" className="error">
+        Required: Get Your Unsplash API Key First
+      </a>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Unsplash Image Gallery!</h1>
+
+      <form onSubmit={searchPhotos}>
+        <input 
+          type="text" 
+          placeholder="Search Unsplash..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button>Search</button>
+      </form>
+
+       <InfiniteScroll
+        dataLength={images.length}
+        next={() => setPage((page) => page + 1)}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+      >
+      <div className="image-grid">
+          {images.map((image, index) => (
+            <a
+              className="image"
+              key={index}
+              href={image.links.html}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={image.urls.regular} alt={image.alt_description} />
+            </a>
+          ))}
+        </div>
+      </InfiniteScroll>
     </div>
   );
 }
-
-export default App;
